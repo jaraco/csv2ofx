0560beb53d5db94506142298644a226b64c8a95f
Jason R. Coombs <jaraco@jaraco.com>
2021-05-25 14:50:02 -0400

    Adding mapping for Amazon purchases.

---
+++ csv2ofx/mappings/amazon.py
@@ -0,0 +1,56 @@
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
+
+import os
+import functools
+import collections
+from operator import itemgetter
+
+
+def all_but_the_last_n(iterable, count):
+    q = collections.deque()
+    i = iter(iterable)
+    for n in range(count):
+        q.append(next(i))
+    for item in i:
+        q.append(item)
+        yield q.popleft()
+
+
+all_but_last = functools.partial(all_but_the_last_n, count=1)
+
+
+def parse_cards(setting):
+    if setting is None:
+        return []
+    return setting.split(',')
+
+
+def exclude_cards(records):
+    setting = os.environ.get('AMAZON_EXCLUDE_CARDS', None)
+    cards = parse_cards(setting)
+    filter_ = lambda row: not any(card in row['payments'] for card in cards)
+    return filter(filter_, records)
+
+
+# from jaraco.functools
+def compose(*funcs):
+    def compose_two(f1, f2):
+        return lambda *args, **kwargs: f1(f2(*args, **kwargs))
+
+    return functools.reduce(compose_two, funcs)
+
+
+mapping = {
+    'has_header': True,
+    'process_records': compose(exclude_cards, all_but_last),
+    'delimiter': ',',
+    'bank': 'Amazon Purchases',
+    'account_id': os.environ.get('AMAZON_PURCHASES_ACCOUNT', '100000001'),
+    'date': itemgetter('date'),
+    'amount': itemgetter('total'),
+    'payee': 'Amazon',
+    'desc': itemgetter('items'),
+    'id': itemgetter('order id'),
+    'type': 'DEBIT',
+}
